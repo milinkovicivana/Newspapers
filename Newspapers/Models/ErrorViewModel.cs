77f1b272ced70c868b136ec560e09777817321cc
ivana <ivanamilinkovic6@gmail.com>
2019-04-28 19:55:34 +0200

    Add project files.

---
+++ Newspapers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Newspapers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
